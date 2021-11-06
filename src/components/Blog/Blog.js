b20d43ca0fcc75b44f0885d576d33b16ac6c74db
times.jugal <times.jugal@gmail.com>
2021-11-06 19:17:53 +0530

    Added icon to Open Blog button

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,17 +1,18 @@
-import Button from "../../styles/GlobalComponents/Button";
-import {Section} from "../../styles/GlobalComponents";
 import React from "react";
-import {FRONT_END_URL} from "../utils";
+import Button from "../../styles/GlobalComponents/Button";
+import { Section } from "../../styles/GlobalComponents";
+import { MdOpenInNew } from "react-icons/md";
+import { FRONT_END_URL } from "../utils";
 
 const Blog = () => {
     return (
-       <Section>
-           <a href={FRONT_END_URL} target={'_blank'}>
-               <Button disableHover>Check out blog!</Button>
-           </a>
-       </Section>
+        <Section>
+            <a href={FRONT_END_URL} target={'_blank'}>
+                <Button disableHover>Check out blog! <MdOpenInNew style={{ marginLeft: 5 }} /></Button>
+            </a>
+        </Section>
 
     )
 }
 
-export default  Blog
+export default Blog
